8e748aeec9a64818ca927ddd479f1b719c105574
higginsdragon <higginsdragon@users.noreply.github.com>
2026-06-26 01:49:38 +0000

    deploy: d9624fbfd84d0191a27a9bef8268cf300d2b598c

--- group__styli.js
+++ group__styli.js
@@ -31,6 +31,7 @@ var group__styli =
     ] ],
     [ "libwacom_get_styli", "group__styli.html#ga18cd23a382eade1bc72b3ef3c49060fb", null ],
     [ "libwacom_get_supported_styli", "group__styli.html#ga3f82f01ec46ca1869eb54e9359970a6e", null ],
+    [ "libwacom_list_styli_from_database", "group__styli.html#ga8024fcd4cf0e0529a602f4ba0c600d76", null ],
     [ "libwacom_print_stylus_description", "group__styli.html#ga84b9c038c3d9f53c8c9fed4ff30f71f2", null ],
     [ "libwacom_stylus_get_axes", "group__styli.html#ga28c212a70b84efb5d06460c95f87d6d2", null ],
     [ "libwacom_stylus_get_eraser_type", "group__styli.html#ga7299684f73159055f5b4e3ceb5b0f4d0", null ],
@@ -45,5 +46,6 @@ var group__styli =
     [ "libwacom_stylus_has_eraser", "group__styli.html#gaa5042d687a5ca7c3b8625d7db1143356", null ],
     [ "libwacom_stylus_has_lens", "group__styli.html#ga3302fa288fae4c8057ae75d4120c075e", null ],
     [ "libwacom_stylus_has_wheel", "group__styli.html#ga25750936d41579fa5cb0d5296e68d7f8", null ],
-    [ "libwacom_stylus_is_eraser", "group__styli.html#gadeebad73b4bce9f9791ef59e07b8c051", null ]
+    [ "libwacom_stylus_is_eraser", "group__styli.html#gadeebad73b4bce9f9791ef59e07b8c051", null ],
+    [ "libwacom_stylus_is_generic", "group__styli.html#gaf3f387a2a713ab790fd40242573617c7", null ]
 ];
